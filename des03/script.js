46dda9b5e46767cf1489310b28f3e774883c7b6c
Anniely Oliveira <annieleoliveirareiis@gmail.com>
2024-07-05 22:07:45 -0300

    Adicionando o desafio 03 - lacos repeticao for

---
+++ des03/script.js
@@ -0,0 +1,13 @@
+
+/* 
+Faça um programa que peça uma nota, entre zero e dez.
+Mostre uma mensagem caso o valor seja inválido e continue pedindo até que o 
+usuário informe um valor válido.  
+*/
+
+// let valor = prompt("Digite um valor")
+
+
+for(let i = 10; i > 0; i--) {
+  console.log("Repetindo o for: " + i)
+}
