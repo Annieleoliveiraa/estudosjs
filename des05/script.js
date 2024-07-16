89d8253b60bb90b510116be1dbb8aa5d143097a6
Anniely Oliveira <annieleoliveirareiis@gmail.com>
2024-07-16 18:29:02 -0300

    Adicionando o exercicio 05 de js

---
+++ des05/script.js
@@ -0,0 +1,14 @@
+
+/* 
+Um comerciante comprou um produto e quer vendê-lo com lucro de 45% 
+se o valor da
+compra for menor que 20,00; caso contrário, o lucro será de 30%. 
+Ler o valor do produto e
+imprimir o valor da venda (conforme as taxas do enunciado).
+*/
+
+
+let dadosUsuario = parseInt(prompt("Insira um número"))
+if(dadosUsuario === "20" || dadosUsuario ) {
+
+}
