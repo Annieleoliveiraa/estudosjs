816805b097c21cd9842aabcaf4f01609b776e07e
Anniele Oliveiraa <annieleoliveiraa@gmail.com>
2024-12-17 20:16:26 -0300

    style: Adiciona o arquivo do exercicio com o js

---
+++ des07/script.js
@@ -0,0 +1,15 @@
+
+/* 
+Faça um programa que leia três notas, 
+calcule e mostre a média aritmética entre elas.
+*/  
+
+let numero1 = Number(prompt('Digite um número'))
+let numero2 = Number(prompt('Digite outro número'))
+let numero3 = Number(prompt('Digite outro número'))
+
+if(numero1 + numero2 + numero3 / ) {
+  
+}
+
+alert('O valor da média aritmética é: ' + numero1 + numero2 + numero3)
