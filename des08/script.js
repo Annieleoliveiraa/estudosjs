fe5503b8b7b3633b76b35ee1599e775d4584c4a7
Anniele Oliveiraa <annieleoliveiraa@gmail.com>
2025-03-29 18:30:50 -0300

    feat: Adicionando exercicio 08 de logica com js

---
+++ des08/script.js
@@ -0,0 +1,13 @@
+
+/* 
+Faça um programa que leia três notas, 
+calcule e mostre a média aritmética entre elas 
+*/
+
+// Criando as variáveis e entrada de dados do usuário 
+let nota1 = Number(prompt("Digite um número"))
+let nota2 = Number(prompt("Digite um segundo número"))
+let nota3 = Number(prompt("Digite um terceiro número"))
+
+// Imprimindo o valor na tela 
+console.log("A média aritmética é: " + (nota1 + nota2 + nota3) / 3)
